7a35634b9fc09b255f322fe85b1b8f5eb02f209c
a20samul <a20samul@student.his.se>
2021-05-13 21:48:27 +0200

    Added an assets folder including about page. Second Activity got deleted.

--- app/src/main/java/com/example/assignment5project/MainActivity.java
+++ app/src/main/java/com/example/assignment5project/MainActivity.java
@@ -8,10 +8,20 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public void showAboutPage(){
+        //.loadUrl("file:///android_asset/about.html");
+
+     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -20,23 +30,39 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.about_page) {
+            showAboutPage();
+            Log.d("==>","Will display About page");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button startButton = findViewById(R.id.enter_button);
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
-                Log.d("TAG", "Start SecondActivity");
-            }
+        //Button startButton = findViewById(R.id.enter_button);
+        //startButton.setOnClickListener(new View.OnClickListener() {
+          //  @Override
+            //public void onClick(View view) {
+              //  Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+               // startActivity(intent);
+                //Log.d("TAG", "Start SecondActivity");
+            //}
 
 
 
-        });
+        //});
 
     }
 }
+
