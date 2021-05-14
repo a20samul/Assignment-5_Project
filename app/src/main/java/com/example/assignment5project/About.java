707d4dfd10ac4c7e87f2a2e54434e69dc6b1a65e
a20samul <a20samul@student.his.se>
2021-05-14 10:01:12 +0200

    Added code for button

--- app/src/main/java/com/example/assignment5project/About.java
+++ app/src/main/java/com/example/assignment5project/About.java
@@ -3,9 +3,12 @@ package com.example.assignment5project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
 
 public class About extends AppCompatActivity {
 
@@ -16,11 +19,21 @@ public class About extends AppCompatActivity {
         setContentView(R.layout.activity_about);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.dropdown_menu, menu);
         return true;
     }
+
+    Button returnButton = findViewById(R.id.return_button);
+    returnButton.setOnClickListener(new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view) {
+            Log.d("END", "Finish app");
+            finish();
+
+        }
+    });
 }
