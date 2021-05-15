8382054c4c322a5dc1b82de2a257d7cfc1effd24
a20samul <a20samul@student.his.se>
2021-05-15 17:44:10 +0200

    Added a java class for the list items + created added ArrayList and ArrayAdapter

--- app/src/main/java/com/example/assignment5project/ListviewActivity.java
+++ app/src/main/java/com/example/assignment5project/ListviewActivity.java
@@ -3,8 +3,14 @@ package com.example.assignment5project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 public class ListviewActivity extends AppCompatActivity {
+
+    private ArrayList<Wonders> items;
+    private ArrayAdapter<Wonders> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/assignment5project/Wonders.java
@@ -0,0 +1,29 @@
+package com.example.assignment5project;
+
+public class Wonders {
+    private String ID;
+    private String name;
+    private String type;
+    private String company;
+    private String location;
+    private String category;
+    private int size;
+    private int cost;
+    private Auxdata auxdata;
+
+    public String getName() { return name;}
+
+    public String getCompany() { return company;}
+
+    public String getLocation() { return location;}
+
+    public String getCategory() { return category;}
+
+    public int getSize() { return size;}
+
+    public Auxdata getAuxdata() { return auxdata;}
+
+
+
+
+}
